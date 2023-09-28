8fb44b51e8083d1de2cbf2bfd8380a28c915d12f
hanjung <hanjungv@gmail.com>
2023-09-28 14:44:08 +0900

    shopping-mall-integration-test-with-answer

--- src/pages/common/components/tests/NavigationBar.spec.jsx
+++ src/pages/common/components/tests/NavigationBar.spec.jsx
@@ -23,12 +23,73 @@ vi.mock('react-router-dom', async () => {
   };
 });
 
-it('"Wish Mart" 텍스트 로고을 클릭할 경우 "/" 경로로 navigate가 호출된다.', async () => {});
+it('"Wish Mart" 사이트 제목을 클릭할 경우 "/" 경로로 navigate가 호출된다.', async () => {
+  const { user } = await render(<NavigationBar />);
+
+  await user.click(screen.getByText('Wish Mart'));
+
+  expect(navigateFn).toHaveBeenNthCalledWith(1, '/');
+});
 
 describe('로그인이 된 경우', () => {
-  beforeEach(() => {});
+  const userId = 10;
 
-  it('장바구니(담긴 상품 수와 버튼)와 로그아웃 버튼(사용자 이름: "Maria")이 노출된다.', async () => {});
+  beforeEach(() => {
+    server.use(
+      rest.get('/user', (_, res, ctx) => {
+        return res(
+          ctx.status(200),
+          ctx.json({
+            email: 'maria@mail.com',
+            id: userId,
+            name: 'Maria',
+            password: '12345',
+          }),
+        );
+      }),
+    );
+    mockUseUserStore({ isLogin: true });
+
+    const cart = {
+      6: {
+        id: 6,
+        title: 'Handmade Cotton Fish',
+        price: 100,
+        description:
+          'The slim & simple Maple Gaming Keyboard from Dev Byte comes with a sleek body and 7- Color RGB LED Back-lighting for smart functionality',
+        images: [
+          'https://user-images.githubusercontent.com/35371660/230712070-afa23da8-1bda-4cc4-9a59-50a263ee629f.png',
+          'https://user-images.githubusercontent.com/35371660/230711992-01a1a621-cb3d-44a7-b499-20e8d0e1a4bc.png',
+          'https://user-images.githubusercontent.com/35371660/230712056-2c468ef4-45c9-4bad-b379-a9a19d9b79a9.png',
+        ],
+        count: 3,
+      },
+      7: {
+        id: 7,
+        title: 'Awesome Concrete Shirt',
+        price: 50,
+        description:
+          'The Nagasaki Lander is the trademarked name of several series of Nagasaki sport bikes, that started with the 1984 ABC800J',
+        images: [
+          'https://user-images.githubusercontent.com/35371660/230762100-b119d836-3c5b-4980-9846-b7d32ea4a08f.png',
+          'https://user-images.githubusercontent.com/35371660/230762118-46d965ab-7ea8-4e8a-9c0f-3ed90f96e1cd.png',
+          'https://user-images.githubusercontent.com/35371660/230762139-002578da-092d-4f34-8cae-2cf3b0dfabe9.png',
+        ],
+        count: 4,
+      },
+    };
+    mockUseCartStore({ cart });
+  });
+
+  it('장바구니(담긴 상품 수와 버튼)와 로그아웃 버튼(사용자 이름: "Maria")이 노출된다.', async () => {
+    await render(<NavigationBar />);
+
+    expect(screen.getByTestId('cart-icon')).toBeInTheDocument();
+    expect(screen.getByText('2')).toBeInTheDocument();
+    expect(
+      await screen.findByRole('button', { name: 'Maria' }),
+    ).toBeInTheDocument();
+  });
 
   it('장바구니 버튼 클릭 시 "/cart" 경로로 navigate를 호출한다.', async () => {
     const { user } = await render(<NavigationBar />);
