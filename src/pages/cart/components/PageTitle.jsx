2154bc701ed416bc49fe6faef694c73c9b5a278b
Dayeon Yoon <yday1223@gmail.com>
2024-04-01 10:10:15 +0900

    snapshot test in utils

--- src/pages/cart/components/PageTitle.jsx
+++ src/pages/cart/components/PageTitle.jsx
@@ -2,7 +2,7 @@ import { Typography } from '@mui/material';
 import React from 'react';
 
 const PageTitle = () => (
-  <Typography variant="h4" component="h1" padding="20px 0">
+  <Typography variant="h4" component="h2" padding="20px 0">
     상품 리스트
   </Typography>
 );
--- src/pages/cart/components/tests/PageTitle.spec.jsx
+++ src/pages/cart/components/tests/PageTitle.spec.jsx
@@ -3,6 +3,27 @@ import React from 'react';
 import PageTitle from '@/pages/cart/components/PageTitle';
 import render from '@/utils/test/render';
 
-it('pageTitle 스냅샷 테스트(toMatchInlineSnapshot)', async () => {});
+// 테스트 파일 내에 스냅샷까지 함께 관리하고 싶은 경우
+it('pageTitle 스냅샷 테스트(toMatchInlineSnapshot)', async () => {
+  const { container } = await render(<PageTitle />);
 
-it('pageTitle 스냅샷 테스트(toMatchSnapshot)', async () => {});
+  expect(container).toMatchInlineSnapshot(`
+    <div>
+      <h2
+        class="MuiTypography-root MuiTypography-h4 css-1lnl64-MuiTypography-root"
+      >
+        상품 리스트
+      </h2>
+      <div
+        style="position: fixed; z-index: 9999; top: 16px; left: 16px; right: 16px; bottom: 16px; pointer-events: none;"
+      />
+    </div>
+  `);
+});
+
+// 테스트 파일과 스냅샷 파일을 별도로 관리하고 싶은 경우
+it('pageTitle 스냅샷 테스트(toMatchSnapshot)', async () => {
+  const { container } = await render(<PageTitle />);
+
+  expect(container).toMatchSnapshot();
+});
