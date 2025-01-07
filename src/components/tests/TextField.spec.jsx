14ab8144ff697de3b45b174d3539600c0dd55a02
HYO JIN LIM <59094619+hyo814@users.noreply.github.com>
2025-01-07 16:48:43 +0900

    Update TextField.spec.jsx
    2.1 강의 주석 추가

--- src/components/tests/TextField.spec.jsx
+++ src/components/tests/TextField.spec.jsx
@@ -5,8 +5,22 @@ import TextField from '@/components/TextField';
 import render from '@/utils/test/render';
 
 it('className prop으로 설정한 css class가 적용된다.', async () => {
-  await render(<TextField className="my-class" />);
+  // Arrange - 테스트를 위한 환경 만들기
+  // -> className을 지닌 컴포넌트 렌더링
+  // Act - 테스트할 동작 발생
+  // -> 렌더링에 대한 검증이기 때문에 이 단계는 생략
+  // -> 클릭이나 메서드 호출, prop 변경 등등에 대한 작업이 여기에 해당
+  // Assert - 올바른 동작이 실행 되었는지 검증
+  // -> 렌더링 후 DOM 에 해당 class 가 존재 하는지 검증
 
+  // render API를 호출 -> 테스트 환경의 jsDOM에 리액트 컴포넌트가 렌더링 된 DOM 구조가 반영
+  // jsDOM : Node.js에서 사용하기 위해 많은 웹 표준을 순수 자바스크립트로 구현
+  await render(<TextField className="my-class" />);
+  
+  // vitest의 expect 함수를 사용하여 기대 결과를 검증
+
+  // className이란 내부 prop나 state 값을 검증 X
+  // 렌더링 되는 DOM 구조가 올바르게 변경 되었는지 검증 -> 최종적으로 사용자가 보는 결과가 어떤 것일지 확인 할 것
   const textInput = screen.getByPlaceholderText('텍스트를 입력해 주세요.');
 
   expect(textInput).toHaveClass('my-class');
